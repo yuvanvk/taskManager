f435c5703bd1cd9ab4bdc069b24486011d95638c
yuvanvk <abhivigneshofficial@gmail.com>
2025-11-29 11:41:06 +0530

    feat: add inputs

--- frontend/src/pages/task-prioritizer.tsx
+++ frontend/src/pages/task-prioritizer.tsx
@@ -1,7 +1,13 @@
-import { Navbar } from "@/components/ui/navbar"
+import { AddTasks } from "@/components/app/add-tasks";
+import { Navbar } from "@/components/ui/navbar";
 
 export const TaskPrioritizer = () => {
-    return <div>
-        <Navbar />
-    </div>
-}
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto">
+        <AddTasks />
+      </div>
+    </>
+  );
+};
